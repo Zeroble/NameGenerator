ec75cf035cdb8162237f3a0569290571c00a1de5
Zeroble <dlalsdyd0219@gmail.com>
2019-07-23 22:27:06 +0900

    3.0 update

--- app/src/main/java/com/sup3rd3v3l0p3r/teamvetor/namegenerator/MainActivity.java
+++ app/src/main/java/com/sup3rd3v3l0p3r/teamvetor/namegenerator/MainActivity.java
@@ -11,6 +11,11 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.ads.mediation.admob.AdMobAdapter;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,8 +48,18 @@ public class MainActivity extends AppCompatActivity {
         sw2.setOnCheckedChangeListener(myCheckedListener);
         sw3.setOnCheckedChangeListener(myCheckedListener);
 
+        adMob();
     }
-
+    private void adMob(){
+        MobileAds.initialize(this, getString(R.string.banner_ad_unit_id));
+        AdView mAdView = findViewById(R.id.adView);
+        Bundle extras = new Bundle();
+//        extras.putString("max_ad_content_rating", "G"); // 앱이 3세 이상 사용가능이라면 광고레벨을 설정해줘야 한다
+        AdRequest adRequest = new AdRequest.Builder()
+                .addNetworkExtrasBundle(AdMobAdapter.class, extras)
+                .build();
+        mAdView.loadAd(adRequest);
+    }
     public void copyButton(View v) {
         ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         ClipData clipData = ClipData.newPlainText("label", one + two + three + four);
@@ -97,37 +112,47 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("asdfasdf",choose+"");
 
-        String[] first = {
+        String[] first = {//-는, -한(현재), -운, -은
                 "물구나무 서기 하는 ", "전율하는 ", "절규하는 ", "사랑하는 ",
-                "게임하는 ", "상처받는 ", "휴지 뽑는 ", "프로그래밍하는 ",
+                "게임하는 ", "상처받은 ", "휴지 뽑는 ", "코딩하는 ",
                 "배구하는 ", "축구하는 ", "행복해하는 ", "필기하는 ", "약속하는 ",
-                "달리는 ", "깨부수는 ", "산책하는 ", "심심해하는 ", "할거없는 ",
-                "과자 먹는 ", "사랑중인 ", "발차기하는 ", "상처받은 ", "애교떠는 ",
+                "달리는 ", "깨부수는 ", "산책하는 ", "심심한 ", "할거없는 ",
+                "과자 먹는 ", "사랑중인 ", "발차기하는 ", "애교떠는 ",
                 "시계 보는 ", "추워하는 ", "충전중인 ", "진정한 ", "완벽한 ", "완전한 ",
-                "영원한 "
+                "영원한 ", "춤추는 ", "등이 간지러운 ", "공부하는 ", "뜨거운 ", "차가운 ",
+                "맛있는 ", "심심한 ", "모두에게 사랑받는 ", "쓰레기같은 ", "무서운 ", "봉인당한 ", "지루한 ",
+                "피곤한 ", "점성있는 ", "평범한 "
 
         };
-        String[] second = {
+        String[] second = {// -된, -한(과거)
                 "감염된 ", "전염된 ", "충전된 ", "변화된 ", "감전된 ", "인증된 ",
-                "정리정돈된 ", "안정된 ", "폭발한 ", "파괴된 ", "사망한 ", "무참히살해된 ",
+                "정리정돈된 ", "안정된 ", "폭발한 ", "파괴된 ", "사망한 ",
                 "무력화된 ", "잉여가된 ", "봉인된 ", "기계화된 ", "자동화된 ", "구속된 ",
                 "포박된 ", "단련된 ", "파멸한 ", "부활한 ", "진화한 ", "속박된 ", "변화된 ",
-                "변신한 ", "폐기된 ", "착취당한 ", "거래된 ", "이상한 "
+                "변신한 ", "폐기된 ", "착취당한 ", "거래된 ", "이상한 ", "변화된 ", "암호화된 ",
+                "검은색 ", "푸른색 ", "노란색 ", "무지개색 ", "하얀색 ", "핑크색 ", "신고당한 "
         };
-        String[] third = {
+        String[] third = {//-의
                 "암흑의 ", "전율의 ", "지옥의 ", "고통의 ", "절망의 ", "대죄의 ", "강철의 ", "불꽃의 ",
                 "고양이 차림의 ", "메이드복 차림의 ", "파멸의 ", "희망의 ", "역습의 ", "역전의 ", "연속 처치의 ",
                 "치유의 ", "심쿵사의 ", "암살자의 ", "소환수의 ", "바보의 ", "분신술의 ", "전설의 ",
-                "전설은 아니고 레전드의", "세번 째", "다섯 번째 ", "일렉트로닉 마스터의 ", "초전자포의 ",
-                "마법의 ", "게임의 ", "마왕의 "
+                "전설은 아니고 레전드의", "세 번째", "다섯 번째 ", "일렉트로닉 마스터의 ", "초전자포의 ",
+                "마법의 ", "게임의 ", "마왕의 ", "마법사의 ", "의문의 ", "심연의 ", "임시의 ", "임의의 ",
+                "사냥꾼의 ", "고룡사냥꾼의 ", "예술가의 ", "공포의 ", "의외의 ", "특급 "
         };
 
         String[] fourth = {
                 "바나나", "악당", "천재", "말미잘", "바보", "TV", "줄자", "스님", "목사님",
-                "휴대폰", "지갑", "두꺼운책", "빠삐코", "볼펜", "오른팔", "왼팔", "새끼손가락",
+                "휴대폰", "지갑", "두꺼운 책", "빠삐코", "볼펜", "오른팔", "왼팔", "새끼손가락",
                 "어둠", "희망", "절망", "로켓", "복사기", "한약", "두루마리 휴지", "노트북",
-                "시계", "자동차", "고양이", "암살자", "컴퓨터", "노트북", "코카콜라"
+                "시계", "자동차", "고양이", "암살자", "컴퓨터", "노트북", "코카콜라", "마추픽추", "초코파이",
+                "이어폰", "우유", "학생", "소녀", "소년", "마왕", "용사", "엑스트라", "코끼리", "그것", "인간",
+                "강아지", "너굴맨", "대머리", "학교", "경찰", "손님", "손", "생명", "남편", "아내", "민족",
+                "하늘", "커피", "음악", "종교", "동생", "짜파게티", "요리사", "하키선수", "축구선수", "키위새",
+                "쓰래기", "대학생", "돼지", "심장", "혈액", "화살", "직장인", "계산기", "죄수", "버섯", "로봇",
+                "고춧가루", "고추참치", "머리카락", "용액", "슬라임", "닉네임", "게임기", "노예"
         };
+        Log.d("asdf", first.length+" "+second.length+" "+third.length+" "+fourth.length);
         if (choose == 4 || choose == 5 || choose == 6 || choose == 7) { // -는
             one = first[((int) (Math.random() * first.length))];
         }
